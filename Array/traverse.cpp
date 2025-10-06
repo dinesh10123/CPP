e6a4c613f3cfa3e420bf4b20c5953466c3072b55
dinesh10123 <159063788+dinesh10123@users.noreply.github.com>
2025-10-06 19:27:21 +0530

    Update traverse.cpp

--- Array/traverse.cpp
+++ Array/traverse.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of Array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements in Array : ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Array elements are : ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
